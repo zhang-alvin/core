756656a82694e32fb4604150147ea7c49aa4eba0
zhang-alvin <zhanga@mastermind.scorec.rpi.edu>
2019-03-12 12:39:38 -0400

    CONFIG: settings

--- example_config.sh
+++ example_config.sh
@@ -1,5 +1,9 @@
 cmake .. \
   -DCMAKE_C_COMPILER="mpicc" \
   -DCMAKE_CXX_COMPILER="mpicxx" \
+  -DENABLE_SIMMETRIX=True \
   -DSIM_MPI="mpich3.1.2" \
-  -DENABLE_ZOLTAN=ON
+  -DCMAKE_INSTALL_PREFIX="/lore/zhanga/core-zhang-alvin/install" \
+  -DENABLE_ZOLTAN=ON \
+  -DSIM_PARASOLID=ON \
+  -DBUILD_SHARED_LIBS=True
